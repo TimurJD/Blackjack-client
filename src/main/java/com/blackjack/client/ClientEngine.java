1cb03fef4d0b05695d08a7635b476e18f37156fc
TimurJD <tima.peaceworld@gmail.com>
2015-12-30 12:09:40 +0200

    Client work flow refactored

--- src/main/java/com/blackjack/client/ClientEngine.java
+++ src/main/java/com/blackjack/client/ClientEngine.java
@@ -1,6 +1,7 @@
 package com.blackjack.client;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -13,74 +14,82 @@ import com.blackjack.model.Hand;
 public class ClientEngine {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 
+		// Stage - 1
 		BlackjackClient client = new BlackjackClient();
 		client.connect();
 
 		Scanner scanner = new Scanner(System.in);
 
+		Map<String, Object> data = new HashMap<String, Object>();
+		
 		try {
-			System.out.println("********** Welcome to the BlackJak tournament **********");
+			System.out.println("********** Welcome to the BlackJak tournament **********\n");
 			while (true) {
 				
-				System.out.println("Your choise: [1 - DEAL]");
+				// Stage - 2
+				System.out.println("\n> Your choise: [1 - DEAL]");
 
 				if (scanner.nextInt() == 1) {
 					client.sendDataToSever(GameStatus.DEAL);
 				}
 
-				Map<String, Object> data = client.getDataFromServer();
+				data = client.getDataFromServer();
 
 				Hand dealerHand = (Hand) data.get("dealerHand");
 				Hand playerHand = (Hand) data.get("playerHand");
 				
-				if (data.get("status") != GameStatus.PLAYER_WON) {
-					System.out.println("Dealer hand is:" + dealerHand + " -> " + dealerHand.getHandScore());
-					System.out.println("Your hand is: " + playerHand + " -> " + playerHand.getHandScore());
-
-					System.out.println("Your choise: [1 - HIT | 2 - STAND]");
-					int option = scanner.nextInt();
-					if (option == 1) {
-						client.sendDataToSever(GameStatus.HIT);
-					} else if (option == 2) {
-						client.sendDataToSever(GameStatus.STAND);
-						System.out.println("Dealer hand is: " + dealerHand + " -> " + dealerHand.getHandScore());
-						
-						if(dealerHand.getHandScore() > playerHand.getHandScore()) {
-							System.out.println("Your hand is: " + playerHand + " -> " + playerHand.getHandScore());
-							System.out.println("You BUST");
-						} else if(dealerHand.getHandScore() < playerHand.getHandScore()) {
-							System.out.println("Your hand is: " + playerHand + " -> " + playerHand.getHandScore());
-							System.out.println("You WON");
-						} else if(dealerHand.getHandScore() == playerHand.getHandScore()) {
+				if (data.get("status") == GameStatus.PLAYER_WON) { // If player hand at start is equals to 21
+					System.out.println("///// DEALER HAND ///// - " + dealerHand + " -> " + dealerHand.getHandScore());
+					System.out.println("///// YOUR HAND ///// - " + playerHand + " -> " + playerHand.getHandScore());
+					System.out.println("***** YOU WON *****");
+					continue;
+				} else {
+					// Stage - 3
+					System.out.println("///// DEALER HAND ///// - " + dealerHand + " -> " + dealerHand.getHandScore());
+					System.out.println("///// YOUR HAND ///// - " + playerHand + " -> " + playerHand.getHandScore());
+					
+					while(true) {
+						System.out.println("\n> Your choise: [1 - HIT | 2 - STAND]");
+						int option = scanner.nextInt();
+						if (option == 1) {
+							client.sendDataToSever(GameStatus.HIT);
 							
-						}
-						continue;
-					}
-
-					while (true) {
-						data = client.getDataFromServer();
-						
-						playerHand = (Hand) data.get("playerHand");
-						System.out.println("Your hand is: " + playerHand + " -> " + playerHand.getHandScore());
-
-						if (data.get("status") == GameStatus.BUST) {
-							System.out.println("You BUST");
-							break;
-						} else if (data.get("status") == GameStatus.PLAYER_WON) {
-							System.out.println("You WON");
-							break;
-						} else {
-							System.out.println("Your choise: [1 - HIT | 2 - STAND]");
-							if (option == 1) {
-								client.sendDataToSever(GameStatus.HIT);
-							} else if (option == 2) {
-								client.sendDataToSever(GameStatus.STAND);
+							data = client.getDataFromServer();
+							
+							dealerHand = (Hand) data.get("dealerHand");
+							playerHand = (Hand) data.get("playerHand");
+							
+							System.out.println("///// DEALER HAND ///// - " + dealerHand + " -> " + dealerHand.getHandScore());
+							System.out.println("///// YOUR HAND ///// - " + playerHand + " -> " + playerHand.getHandScore());
+	
+							if (data.get("status") == GameStatus.PLAYER_WON) { // If player hand at start is equals to 21
+								System.out.println("***** YOU WON AAAAAA *****");
+								break;
+							} else if(data.get("status") == GameStatus.BUST) {
+								System.out.println("***** YOU BUSTED *****");
+								break;
+							} else {
+								continue;
+							}
+						} else if (option == 2) {
+							client.sendDataToSever(GameStatus.STAND);
+							dealerHand = (Hand) client.getDataFromServer().get("dealerHand");
+							
+							System.out.println("///// DEALER HAND ///// - " + dealerHand + " -> " + dealerHand.getHandScore());
+							System.out.println("///// YOUR HAND ///// - " + playerHand + " -> " + playerHand.getHandScore());
+							
+							if(dealerHand.getHandScore() > playerHand.getHandScore()) {
+								System.out.println("You BUST");
+								break;
+							} else if(dealerHand.getHandScore() < playerHand.getHandScore()) {
+								System.out.println("You WON HERE");
+								break;
+							} else if(dealerHand.getHandScore() == playerHand.getHandScore()) {
+								System.out.println("DRAW");
+								break;
 							}
 						}
 					}
-				} else {
-					System.out.println(playerHand);
-					System.out.println("YOU WON");
 				}
 			}
 		} catch (IOException e) {
